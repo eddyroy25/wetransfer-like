4e2ba8ab61daaf2fc64becc67a135b7ac3d1679e
Unknown <eddyroy2125@gmail.com>
2017-02-21 11:26:17 +0100

    Upload des fichiers sur serveur ok. récupération nom + extension ok

--- Controller/script.php
+++ Controller/script.php
@@ -24,7 +24,7 @@ $erreur = false;
     }
 	
    if ( (isset($_POST["mail_exp"])) && (strlen(trim($_POST["mail_exp"])) > 0) && (filter_var($_POST["mail_exp"], FILTER_VALIDATE_EMAIL)) ) {
-        $expediteur = stripslashes(strip_tags($_POST["Email"]));
+        $expediteur = stripslashes(strip_tags($_POST["mail_exp"]));
     } elseif (empty($_POST["mail_dest"])) {
         $_SESSION['mail_dest_erreur'] = "Merci d'écrire une adresse email";
         $expediteur = "";
@@ -37,23 +37,23 @@ $erreur = false;
 	
 	if ($erreur == false) {
 		$dest = $_POST['mail_dest'];
-		$objet        = "Fichier à télécharger sur Ostifly !";
-		$contenu      = "Mail de l'expéditeur l'expéditeur : " .$_POST['mail_exp']. "\r\n";
+		$objet        	= "Fichier à télécharger sur Ostifly !";
+		$contenu      	= "Mail de l'expéditeur l'expéditeur : " .$_POST['mail_exp']. "\r\n";
 		$contenu     	="Bonjour ! \r\n Vous avez reçu une invition de la part de".$_POST['mail_exp']." pour télécharger un fichier. Nous vous invitons à cliquer sur le lien suivant pour le télécharger : lien . Bonne journée et à bientôt ! L'équipe d'Ostifly.\r\n\n";
-	 
-		$headers .= "Content-Type: text/plain; charset=\"utf-8\"; DelSp=\"Yes\"; format=flowed /r/n";
-		$headers .= "Content-Disposition: inline \r\n";
-		$headers .= "Content-Transfer-Encoding: 7bit \r\n";
-		$headers .= "MIME-Version: 1.0";
-	 
-		// if ( (empty($_FILES["fichier"]['name'])) && (empty($_POST["mail_dest"])) && (empty($_POST["mail_exp"]))) {
-			// echo 'Erreur <br /><a href="../index.html">Retour</a>';
-		// }
-		
+		$headers 		= "Content-Type: text/plain; charset=\"utf-8\"; DelSp=\"Yes\"; format=flowed /r/n";
+		$headers 		.= "Content-Disposition: inline \r\n";
+		$headers 		.= "Content-Transfer-Encoding: 7bit \r\n";
+		$headers 		.= "MIME-Version: 1.0";
+	
+			$target = "../Views/";
+			
+			$file_tmp = $_FILES["fichier"]['tmp_name'];
+			$file = $_FILES["fichier"]['name'];
+			$extension = pathinfo($file,PATHINFO_EXTENSION);
+			var_dump($extension);
+			$files = $file.".".$extension;
+			move_uploaded_file($file_tmp, $target.$file);
 			mail($dest, $objet, utf8_decode($contenu), $headers);
-			$target = "Views/";
-			$file = $_FILES["fichier"]['tmp_name'];
-			move_uploaded_file($target,$file);
 	}
 	
-	 header('Location: ../index.php');
+	 // header('Location: ../index.php');
