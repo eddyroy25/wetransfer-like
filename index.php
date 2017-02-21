1c65ac1666b3c2c88ea02af458452edacec68ebc
Mathieucst <cressot.mathieu@gmail.com>
2017-02-21 14:08:48 +0100

    Merge branch 'master' of C:\Users\Mcst\PhpstormProjects\wetransfer-like with conflicts.

--- index.php
+++ index.php
@@ -6,33 +6,45 @@ session_start();
 <html>
     <head>
         <meta charset="utf-8">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"
+              integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" href="Views/css/bootstrap.min.css">
+
         <link rel="stylesheet" href="Views/css/style.css">
 
         <title>we transfer like</title>
     </head>
     <body>
         <header>
-            <img class ="logo" src="Views/images/ostrich.svg" alt="">
+            <img src="Views/images/ostrich.png" alt="">
         </header>
-        <main>
-            <form id="formulaire" enctype="multipart/form-data" class="form" action="Controller/script.php" method="post">
-				<input type="hidden" name="MAX_FILE_SIZE" value="300000000000" />
-                <input id="fichier" type="file" name="fichier" value="">
-				<span  id="fichier_err"> <?php echo $_SESSION["fichier_erreur"];?></span>
-                <input id="mail_dest" type="text" name="mail_dest" value="">
-				<span  id="mail_dest_err"> <?php echo $_SESSION["mail_dest_erreur"];?></span>
-                <input id="mail_exp" type="text" name="mail_exp" value="">
-				<span  id="mail_exp_err"> <?php echo $_SESSION["mail_exp_erreur"];?></span>
-                <input type="submit" name="envoyer" value="">
-				<span  id="alerte"></span>
-            </form>
-        </main>
+        <div class="container">
+            <div>
+                <div>
+                    <main>
+                        <form id="formulaire" enctype="multipart/form-data" class="form" action="Controller/script.php" method="post">
+                            <input type="hidden" name="MAX_FILE_SIZE" value="300000000000" />
+                            <input id="fichier" type="file" name="fichier" value="">
+                            <span  id="fichier_err"> <?php echo $_SESSION["fichier_erreur"];?></span>
+                            <input id="mail_dest" type="text" name="mail_dest" value="">
+                            <span  id="mail_dest_err"> <?php echo $_SESSION["mail_dest_erreur"];?></span>
+                            <input id="mail_exp" type="text" name="mail_exp" value="">
+                            <span  id="mail_exp_err"> <?php echo $_SESSION["mail_exp_erreur"];?></span>
+                            <input type="submit" name="envoyer" value="">
+                            <span  id="alerte"></span>
+                        </form>
+                    </main>
+                </div>
+            </div>
+        </div>
+
+
+
 <?php
 	session_unset();
 	session_destroy();
 ?>
+        <script src="/view/js/main.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.1/TweenMax.min.js"></script>
-        <script src="/Views/js/main.js"></script>
     </body>
 </html>
