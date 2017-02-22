9a56c0423c6601841df916020cdf5d0beb2b0403
Unknown <eddyroy2125@gmail.com>
2017-02-22 16:10:10 +0100

    Modification $target in script.php

--- Controller/script.php
+++ Controller/script.php
@@ -45,7 +45,7 @@ if ($erreur == false) {
     $headers 		.= "Content-Transfer-Encoding: 7bit \r\n";
     $headers 		.= "MIME-Version: 1.0";
 
-    $target = "http://quangb.marmier.codeur.online/wetransfer-like/Downloads";
+    $target = "../Downloads/";
 
     $file_tmp = $_FILES["fichier"]['tmp_name'];
     $file = $_FILES["fichier"]['name'];
